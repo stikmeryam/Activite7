5665cc35feee36d04c5788c78214161057137ef3
stikmeryam <93292274+stikmeryam@users.noreply.github.com>
2024-01-28 20:32:21 +0100

    Delete spring-angular-oauth2-oidc-keycloak-google-github-project-main-master directory

--- spring-angular-oauth2-oidc-keycloak-google-github-project-main-master/customer-front-thymeleaf-app/src/main/java/org/sid/customerfrontthymeleafapp/repository/CustomerRepository.java
+++
@@ -1,7 +0,0 @@
-package org.sid.customerfrontthymeleafapp.repository;
-
-import org.sid.customerfrontthymeleafapp.entities.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CustomerRepository extends JpaRepository<Customer,Long> {
-}
--- spring-angular-oauth2-oidc-keycloak-google-github-project-main-master/inv-service/src/main/java/org/sid/invservice/repository/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package org.sid.invservice.repository;
-
-import org.sid.invservice.entities.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product,String> {
-}
